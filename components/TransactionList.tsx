77e5cac206b08620f5d273b5c2fa323dd500eb58
rheniumNV <rhenium750@gmail.com>
2024-04-23 19:20:39 +0900

    Refactor TransactionList component to improve performance and readability

--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -1,8 +1,88 @@
 import {Transaction, useApplication} from "@/contexts/Application";
 import {Box, Card, CardContent, Typography} from "@mui/material";
 import {useApi} from "@/contexts/Api";
-import {useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {Zou} from "@/components/Zou";
+
+const usernameCache = new Map<
+    string,
+    Promise<{ data: string; updatedAt: number }>
+>();
+const useUsername = (userId: string) => {
+    const api = useApi();
+    const [username, setUserName] = useState<string>();
+    useEffect(() => {
+        if (userId.startsWith("U-")) {
+            (async () => {
+                try {
+                    const cachePromise = usernameCache.get(userId);
+                    const cached = cachePromise ? await cachePromise : undefined;
+
+                    if (cached && Date.now() - cached.updatedAt < 1000 * 60 * 60) {
+                        setUserName(cached.data);
+                        return;
+                    }
+
+                    const request = (async () => {
+                        try {
+                            const { username } = await api.getResoniteUserDataFromUserId(
+                                userId
+                            );
+                            return { data: username, updatedAt: Date.now() };
+                        } catch (e) {
+                            console.error(e);
+                        return { data: userId, updatedAt: Date.now() };
+                        }
+                    })();
+
+                    usernameCache.set(userId, request);
+
+                    setUserName((await request).data);
+                } catch (e) {
+                    console.error(e);
+                }
+            })();
+        }
+    }, [api, userId]);
+
+    return username ?? userId;
+};
+
+
+function TransactionElement(props: {
+    senderUserId: string;
+    transaction: Transaction;
+  }) {
+    const app = useApplication();
+    const username = useUsername(
+      props.transaction.senderUserId === props.senderUserId
+        ? props.transaction.recipient.resoniteUserId
+        : props.transaction.sender.resoniteUserId
+    );
+  
+    return (
+        <Card key={props.transaction.id}>
+            <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                {new Date(props.transaction.createdAt).toLocaleString()}
+            </Typography>
+            <Typography sx={{ fontSize: 14 }}>
+                {props.transaction.senderUserId === app.userInfo?.id
+                ? "送金"
+                : "受取"}
+            </Typography>
+            <Typography>{username}</Typography>
+            <Typography>
+                {props.transaction.amount} <Zou width={"18px"} height={"18px"} />
+            </Typography>
+            {props.transaction.externalData?.memo && (
+                <Typography>{props.transaction.externalData?.memo}</Typography>
+            )}
+            </CardContent>
+        </Card>
+    );
+  }
+
 
 export type TransactionListProps = {
     incomingTransfers: Transaction[];
@@ -13,72 +93,23 @@ export type TransactionListProps = {
 
 function TransactionList(props: TransactionListProps) {
     const app = useApplication();
-    const api = useApi();
-
-    const [userNames, setUserNames] = useState<any>({});
 
     const {incomingTransfers, outgoingTransfers} = props;
-    // merge incoming and outgoing transfers
-    const transactions = incomingTransfers.concat(outgoingTransfers);
-    // sort by createdAt
-    transactions.sort((a, b) => {
-        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-    })
-
-    //最近５個の取引を表示
-    transactions.splice(5);
-
-    const fetchUsernames = async (transactions: Transaction[]) => {
-        const ids = transactions.map(t => t.senderUserId === app.userInfo?.id ? t.recipient.resoniteUserId : t.sender.resoniteUserId);
-        const uniqueIds = Array.from(new Set(ids));
-        const names : any = {};
-
-        await Promise.all(uniqueIds.map(async (id) => {
-            if(id.startsWith("U-")) {
-                const result = await api.getResoniteUserDataFromUserId(id);
-                names[id] = result.username;
-            } else {
-                names[id] = id;
-            }
-        }));
-
-        setUserNames(names); // 状態更新
-    };
-
-    fetchUsernames(transactions);
-
-    const getUsernameFromId = (id: string) => {
-        return userNames[id] || id; // キャッシュされた名前またはIDを返す
-    };
-
+    const transactions = useMemo(()=>
+        incomingTransfers.concat(outgoingTransfers).sort((a, b) => {
+            return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+        }).slice(0, 5)
+    , [incomingTransfers, outgoingTransfers]);
 
     return (
         <Box display={"flex"} flexDirection={"column"} gap={2}>
             {transactions.map((transaction) => {
                 return (
-                    <Card key={transaction.id}>
-                        <CardContent>
-                            <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
-                                {new Date(transaction.createdAt).toLocaleString()}
-                            </Typography>
-                            <Typography sx={{fontSize: 14}}>
-                                {transaction.senderUserId === app.userInfo?.id ? "送金" : "受取"}
-                            </Typography>
-                            <Typography>
-                                {transaction.senderUserId === app.userInfo?.id ?
-                                    getUsernameFromId(transaction.recipient.resoniteUserId) :
-                                    getUsernameFromId(transaction.sender.resoniteUserId) }
-                            </Typography>
-                            <Typography>
-                                {transaction.amount} <Zou width={"18px"} height={"18px"}/>
-                            </Typography>
-                            { transaction.externalData?.memo &&
-                                <Typography>
-                                    {transaction.externalData?.memo}
-                                </Typography>
-                            }
-                        </CardContent>
-                    </Card>
+                    <TransactionElement
+                        key={transaction.id}
+                        senderUserId={app.userInfo?.id || ""}
+                        transaction={transaction}
+                    />
                 );
             })}
         </Box>
