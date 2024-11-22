9434736437021d902331970beae4b7071774744b
rassi0429 <taitokokoa+rassi@gmail.com>
2024-11-22 09:24:10 +0900

    w

--- lib/api.ts
+++ lib/api.ts
@@ -1,98 +1,98 @@
-import { UserInfo } from "@/contexts/Application";
+import {UserInfo} from "@/contexts/Application";
 
 export class ApiClient {
-  constructor(_endpoint?: string) {
-    console.log("oOoOoOoOoO API SERVICE CREATED OoOoOoOoOo");
-  }
-
-  async getUserInfo() {
-    try {
-      const result = await fetch(`/api/user`, {
-        credentials: "include",
-      });
-      if (result.status === 401) {
-        return null;
-      }
-      return await result.json();
-    } catch (e) {
-      return null;
+    constructor(_endpoint?: string) {
+        console.log("oOoOoOoOoO API SERVICE CREATED OoOoOoOoOo");
     }
-  }
 
-  async login(RLToken: string) {
-    console.log("login with RLToken");
-    try {
-      const result = await fetch(`/api/auth`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${RLToken}`,
-        },
-      });
-      const r = await result.json();
-      console.log(r);
-      return r;
-    } catch (e) {
-      return null;
+    async getUserInfo() {
+        try {
+            const result = await fetch(`/api/user`, {
+                credentials: "include",
+            });
+            if (result.status === 401) {
+                return null;
+            }
+            return await result.json();
+        } catch (e) {
+            return null;
+        }
     }
-  }
 
-  async logout() {
-    try {
-      const result = await fetch(`/api/auth`, {
-        method: "DELETE",
-        credentials: "include",
-      });
-      return result.status === 204;
-    } catch (e) {
-      return false;
+    async login(RLToken: string) {
+        console.log("login with RLToken");
+        try {
+            const result = await fetch(`/api/auth`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${RLToken}`,
+                },
+            });
+            const r = await result.json();
+            console.log(r);
+            return r;
+        } catch (e) {
+            return null;
+        }
     }
-  }
 
-  async updateUserInfo(
-    userInfo: Pick<UserInfo, "branchName" | "accountNumber">,
-  ) {
-    try {
-      const result = await fetch(`/api/user`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userInfo),
-      });
-      return result.status === 200;
-    } catch (e) {
-      return false;
+    async logout() {
+        try {
+            const result = await fetch(`/api/auth`, {
+                method: "DELETE",
+                credentials: "include",
+            });
+            return result.status === 204;
+        } catch (e) {
+            return false;
+        }
     }
-  }
 
-  async sendTransfer(data: {
-    senderId: string;
-    recipientId: string;
-    amount: number;
-    memo?: string;
-  }) {
-    try {
-      const result = await fetch(`/api/transaction`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-      return result.status === 200 || result.status === 201;
-    } catch (e) {
-      return false;
+    async updateUserInfo(
+        userInfo: Pick<UserInfo, "branchName" | "accountNumber">,
+    ) {
+        try {
+            const result = await fetch(`/api/user`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(userInfo),
+            });
+            return result.status === 200;
+        } catch (e) {
+            return false;
+        }
     }
-  }
 
-  async getResoniteUserDataFromUserId(userId: string) {
-    if (!userId.startsWith("U-")) return null;
-    try {
-      const result = await fetch(`/api/proxy/resonite/users/${userId}`);
-      return await result.json();
-    } catch (e) {
-      return null;
+    async sendTransfer(data: {
+        senderId: string;
+        recipientId: string;
+        amount: number;
+        memo?: string;
+    }, customTransactionId?: string) {
+        try {
+            const result = await fetch(`/api/transaction`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({...data, customData: {customTransactionId}}),
+            });
+            return result.status === 200 || result.status === 201;
+        } catch (e) {
+            return false;
+        }
     }
-  }
+
+    async getResoniteUserDataFromUserId(userId: string) {
+        if (!userId.startsWith("U-")) return null;
+        try {
+            const result = await fetch(`/api/proxy/resonite/users/${userId}`);
+            return await result.json();
+        } catch (e) {
+            return null;
+        }
+    }
 }
